d3e2b81a8f49b32b2d781d4a326632422a50f4a3
M4cs <maxlikescs@gmail.com>
2018-11-15 01:15:53 -0500

    Add images and fix requirements. -0.0.3 update

--- babysploit/tutorial.py
+++ babysploit/tutorial.py
@@ -1,5 +1,6 @@
 def run():
     from pyfiglet import Figlet
+    import os
     f = Figlet(style='slant')
     f.renderText("Tutorial")
     start = """
@@ -9,6 +10,25 @@ In this tutorial you will learn how to navigate and use
 BabySploit. The framework is geared towards beginners so
 it shouldn't be too hard to get the hang of things.
     """
+    animated(start)
+    input("Press Enter To Continue...")
+    os.system("clear")
+    second = """
+What is BabySploit?
+
+BabySploit is a penetration testing/social engineering 
+framework aimed towards beginners in the field. Any person,
+with any amount of experience may find use out of BabySploit.
+The framework comes equipped with different types of tools
+use for Information Gathering, Cryptology, Exploitation, and more.
+
+BabySploit is actively being developed and will constantly be
+gaining more and more tools. I plan to spread the framework out
+to more subcategories of tools and creating an extremely large
+tool box that will be able to do a multitude of things but very
+easily for the end user. That is the main goal of BabySploit
+    """
+    animated(second)
     input("Press Enter To Continue...")
 
 def animated(msg):
