f28b1e047e63a3cfff5d4b632803968bfb41bb82
M4cs <maxlikescs@gmail.com>
2018-11-15 14:04:21 -0500

    Remove import os from pdfmeta

--- babysploit/pdfmeta.py
+++ babysploit/pdfmeta.py
@@ -1,6 +1,6 @@
 def start():
     from PyPDF3 import PdfFileReader
-    import os, glob
+    import glob
     print("Put PDF file in pdfs/")
     print("Which PDF file would you like to read the meta data for?")
     for d in glob.iglob("pdfs/*"):
