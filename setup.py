10f08628df225383c7e99592f6029a08af99d707
M4cs <maxlikescs@gmail.com>
2018-11-28 09:59:46 -0500

    Fix Babyrage Error

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description += '\n\n'
 
 setup(
     name='babysploit',
-    version='1.1.18',
+    version='1.1.19',
     author='Max Bridgland',
     author_email='mabridgland@protonmail.com',
     description='Beginner Pentesting Toolkit/Framework',
